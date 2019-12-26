2e0e563a72f1973da3f0688bf1ee71e51028ca0c
Sid Kandasamy <sidkandasamy@outlook.com>
2019-12-26 10:27:39 +0000

    function created for second hand

---
+++ app.js
@@ -0,0 +1,5 @@
+setInterval(setClock, 1000) //this function is called every 1 second
+
+function setClock() {
+    const currentDate = new Date(); //this gives you the current date including time
+}
