6781eeabb77072efe48c0f827504136b3961b2cb
Sid Kandasamy <sidkandasamy@outlook.com>
2019-12-26 10:30:58 +0000

    logic for seconds hand

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ setInterval(setClock, 1000) //this function is called every 1 second
 
 function setClock() {
     const currentDate = new Date(); //this gives you the current date including time
+    const seconds = currentDate.getSeconds() / 60 // get seconds from the current date, we / by 60 to know how far to rotate seconds hand
 }
