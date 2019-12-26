2557a61e1defdee7716b61232dec89cd91984b4e
Sid Kandasamy <sidkandasamy@outlook.com>
2019-12-26 10:35:59 +0000

    logic for all hands

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ setInterval(setClock, 1000) //this function is called every 1 second
 function setClock() {
     const currentDate = new Date(); //this gives you the current date including time
     const seconds = currentDate.getSeconds() / 60 // get seconds from the current date, we / by 60 to know how far to rotate seconds hand
+    const minutes = (seconds + currentDate.getMinutes()) / 60 //seconds is a % of the minute
+    const hour = (minutes + currentDate.getHours()) / 12 // 12 hours on the clock not 60
 }
