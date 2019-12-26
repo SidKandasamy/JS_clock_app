5dfada590d74908e18e2d08be2a804e6a42abb63
Sid Kandasamy <sidkandasamy@outlook.com>
2019-12-26 11:05:31 +0000

    fixed logic for rotation, error in variable in css

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const hourHand = document.querySelector('[data-hour-hand]');
 const hourMinute = document.querySelector('[data-minute-hand]');
 const hourSecond = document.querySelector('[data-second-hand]');
 
+
+
 //https://developer.mozilla.org/en-US/docs/Web/API/Element/querySelectorAll
 
 function setClock() {
     const currentDate = new Date(); //this gives you the current date including time
     const seconds = currentDate.getSeconds() / 60 // get seconds from the current date, we / by 60 to know how far to rotate seconds hand
     const minutes = (seconds + currentDate.getMinutes()) / 60 //seconds is a % of the minute
-    const hour = (minutes + currentDate.getHours()) / 12 // 12 hours on the clock not 60
-    setRotation(hourHand, hour)
-    setRotation(hourMinute, minutes)
+    const hours = (minutes + currentDate.getHours()) / 12 // 12 hours on the clock not 60
     setRotation(hourSecond, seconds)
+    setRotation(hourMinute, minutes)
+    setRotation(hourHand, hours)
 }
 //creating a function which uses the targeting hands
 
